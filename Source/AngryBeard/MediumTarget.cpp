1f4ac8047866303e241a79ceecefb889ee306ae0
Gotvna <nvogt@gaming.tech>
2025-03-25 09:46:14 +0100

    Add onhit function + increase + create bp from c++

--- Source/AngryBeard/MediumTarget.cpp
+++ Source/AngryBeard/MediumTarget.cpp
@@ -4,5 +4,24 @@
 
 int32 AMediumTarget::GetBaseScore() const
 {
-	return 2;
+	return 20;
+}
+
+void AMediumTarget::IncrementScore()
+{
+	AAngryGameMode* GameMode = Cast<AAngryGameMode>(UGameplayStatics::GetGameMode(GetWorld()));
+	if (GameMode)
+	{
+		GameMode->AddScore(GetBaseScore());
+	}
+}
+
+void AMediumTarget::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && OtherActor->ActorHasTag("Bullet"))
+	{
+		IncrementScore();
+		Destroy();
+	}
 }
